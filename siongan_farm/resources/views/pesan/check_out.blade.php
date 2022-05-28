3c8f5d5b15c5bb83492c2bccecf6f92c9c4aaae6
MohIskandarZulkarnain <71996971+MohIskandarZulkarnain@users.noreply.github.com>
2022-05-28 17:22:39 +0700

    28Mei2022

--- siongan_farm/resources/views/pesan/check_out.blade.php
+++ siongan_farm/resources/views/pesan/check_out.blade.php
@@ -25,7 +25,7 @@
 
         <style type="text/css">
             .logo-menu-wrapper{
-            background-color: blue;
+            background-color: #00245b;
             font-family: "Open Sans", sans-serif;
             font-size: 12px;
             line-height: 29px;
